784ec94dc61c1873138b1d1f8881e6b703219edd
Eduardo Seibt <seibteduardo@gmail.com>
2024-04-09 10:28:25 -0300

    some changes

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -16,28 +16,28 @@ export async function GET(req: Request){
     }
 }
 
-// export async function POST(req: Request){
-//     try{
-//         const { horarioTemperatura, grausTemp, horarioAerador, horarioTratador  } = await req.json();
+export async function POST(req: Request){
+    try{
+        const { horarioTemperatura, grausTemp, horarioAerador, horarioTratador  } = await req.json();
 
-//         const lastLog = await prisma.logs.findFirst({
-//             orderBy: {
-//                 horarioTemperatura: "desc",
-//             },
-//         });
+        const lastLog = await prisma.logs.findFirst({
+            orderBy: {
+                horarioTemperatura: "desc",
+            },
+        });
 
-//         const log = await prisma.logs.create({
-//             data: {
-//                 horarioTemperatura: ((horarioTemperatura) > 0 ? new Date().toISOString() : lastLog?.horarioTemperatura) as Date,
-//                 grausTemp,
-//                 horarioAerador: ((horarioAerador) > 0 ? new Date().toISOString() : lastLog?.horarioAerador) as Date,
-//                 horarioTratador: ((horarioTratador) > 0 ? new Date().toISOString() : lastLog?.horarioTratador) as Date,
-//             },
-//         });
+        const log = await prisma.logs.create({
+            data: {
+                horarioTemperatura: ((horarioTemperatura) > 0 ? new Date().toISOString() : lastLog?.horarioTemperatura) as Date,
+                grausTemp,
+                horarioAerador: ((horarioAerador) > 0 ? new Date().toISOString() : lastLog?.horarioAerador) as Date,
+                horarioTratador: ((horarioTratador) > 0 ? new Date().toISOString() : lastLog?.horarioTratador) as Date,
+            },
+        });
 
-//         return NextResponse.json({ log });
-//     } catch (error){
-//         console.error(error);
-//         return NextResponse.json({ message: "Erro ao criar log" }, { status: 500 });
-//     }
-// }
+        return NextResponse.json({ log });
+    } catch (error){
+        console.error(error);
+        return NextResponse.json({ message: "Erro ao criar log" }, { status: 500 });
+    }
+}
